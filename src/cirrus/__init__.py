d5c8a4f7deba265e16ae5add700e913bd90eb1dd
Sean Hudgston <shudgston@gmail.com>
2018-02-21 15:50:54 -0500

    Create release 1.1.0 manually

--- src/cirrus/__init__.py
+++ src/cirrus/__init__.py
@@ -18,4 +18,5 @@ Copyright 2013 Dave Evans
    See the License for the specific language governing permissions and
    limitations under the License.
 """
-__version__="1.0.3"
+__version__="1.1.0"
+
